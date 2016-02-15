932fe8af1b2c1b31cdf369c31df112253c031097
hypemichael <hypemichael@users.noreply.github.com>
2016-02-15 16:51:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var http = require("http");
 var fs= require("fs");
 var url = require("url");
 var path = require("path");
+var page = "./htmlpage.html";
 var port = process.env.PORT || 3000;
 
 var server = http.createServer(function(req,res){
 	var root = __dirname;
     var urlparse = url.parse(req.url).pathname;
 	var joinpath = path.join(root,urlparse);
-	var readfile = fs.createReadStream(joinpath);
+	var readfile = fs.createReadStream(page);
 readfile.pipe(res);
 });
 
