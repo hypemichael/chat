de089079e3e7d3f5e43fa5364c52a9fa5b5a32e8
hypemichael <hypemichael@yahoo.com>
2016-02-18 13:44:39 -0500

    new things added

--- socketserver.js
+++ socketserver.js
@@ -1,11 +1,17 @@
 var socketio = require('socket.io');
 var io;
 var currentRoom={};
+var Guest = 0;
+var User ={};
 exports.listen = function(server){
 	io = socketio.listen(server);
+     io.set('log level', 1);
 	io.sockets.on ('connection',function(socket){
-		joinRoom(socket,'lobby');
-		handleMess(socket);
+		Guest = Guest + 1;
+		User[socket.id]=Guest;
+		joinRoom(socket,'lobby',User[socket.id]);
+		handleMess(socket,Guest);
+		
 	});
 };
 
@@ -13,13 +19,14 @@ exports.listen = function(server){
 
 function handleMess(socket){
 	socket.on("data",function(data){
-		socket.broadcast.to('lobby').emit("data",data);
+		socket.broadcast.to('lobby').emit("data",data,User[socket.id]);
 			console.log(data);
 	})
 }
 
-function joinRoom(socket,room){
+function joinRoom(socket,room,users){
 	socket.join(room);
 	currentRoom[socket.id] = room;
 	console.log(room);
+	socket.broadcast.to('lobby').emit("users",users);
 }
